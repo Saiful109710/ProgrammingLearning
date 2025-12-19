9e209bebf71e1182c89e70862b4755c985a24cb0
saiful-islam <saifulislam109710@gmail.com>
2025-12-19 20:59:11 +0600

    learn take input and typecasting

---
+++ input.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cin >> x;
+    char c;
+    cin >> c;
+    
+    //typecasting
+
+    cout<< "You entered: " << (char)x << endl;
+    cout << "Character: " << (int)c << endl;
+    return 0;
+}
--- print.cpp
+++ print.cpp
@@ -3,6 +3,6 @@ int main(){
     int x = 10;
     char c = 'A';
     double d = 3.21;
-    std:: cout << x << " " << "hello this is goru " << c << " " << d << std:: endl;
+    std:: cout << x << " " << "hello this is goru " << c << std:: endl << d << std:: endl;
     return 0;
 }

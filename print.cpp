6bbdc674dc27e47a1df30622430e1b2c0a9be838
saiful-islam <saifulislam109710@gmail.com>
2025-12-19 20:36:33 +0600

    learn how to print in c++

---
+++ print.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int main(){
+    int x = 10;
+    char c = 'A';
+    double d = 3.21;
+    std:: cout << x << " " << "hello this is goru " << c << " " << d << std:: endl;
+    return 0;
+}

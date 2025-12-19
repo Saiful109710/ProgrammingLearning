1836d15b041d2b45109bbe486b2877bf798b025a
saiful-islam <saifulislam109710@gmail.com>
2025-12-19 22:05:33 +0600

    ternary operator

--- if_else.cpp
+++ if_else.cpp
@@ -2,10 +2,6 @@
 using namespace std;
 int main(){
     int x = 5;
-
-    if(x%2==0){
-        cout << "Even\n";
-    }else{
-        cout << "Odd\n";
-    }
+// ternary operator
+   x%2==0 ? cout << "Even" :cout << "Odd";
 }
